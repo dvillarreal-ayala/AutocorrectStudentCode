afb2642d3243ca4ee11be9f61b41319f8eaf75ad
damianayala <damian.villarreal-ayala@menloschool.org>
2025-03-18 11:19:19 -0700

    commit

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,13 @@
-Name: YOUR NAME HERE
+Name: Damian Villarreal-Ayala
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date    |  Time   |                                                                                                                                            Update |
+|:--------|:-------:|--------------------------------------------------------------------------------------------------------------------------------------------------:|
+| April 5 | 9-10pm  |                                                                                                        I tried to write abstract, efficient code. |
+|         |         |                                                                                                                                                   |
+| Mar 11  |         |                                 Spent time thinking about the problem set and trying to think through different ways to solve while taking notes. |
+| Mar 12  |         |                                                              Took more notes on what Mr. Blick was teaching in class and began writing down code. |
+| Mar 14  |         | Added more to notes and created the LevenshteinDistance method. Need to add more and change base cases, I'm not sure if they're correct right now |
+| Mar 18  | 11-12pm |                                                                                                                                                   |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/Autocorrect.java
+++ src/Autocorrect.java
@@ -8,24 +8,78 @@ import java.io.IOException;
  * A command-line tool to suggest similar words when given one not in the dictionary.
  * </p>
  * @author Zach Blick
- * @author YOUR NAME HERE
+ * @author Damian  Villarreal-Ayala
+ *
+ * NOTES SECTION:
+ * Potential ideas for checking differences:
+ * - Count the num of changes that are present between the two words
+ * ("you", "your", 1 change)
+ *
+ * - Using Levenshtein distance = edit distance
+ * - Base Cases:
+ *   - if length of String b = 0, distance = length of String a.
+ *   - if length of String a = 0, distance = length of String b.
+ *   - if last character of both strings is the same, distance is distance between strings - last character.
+ *   - else, distance will be 1 + the minimum distance of the square to the left, above, and left diagonalally.
+ *
+ * - Can be solved using both tabulation and memoization
+ *
+ * - Currently using substring, but better to replace with something more efficient
+ *  - Tries? Hashing or smth?
+ *
+ *  - Sort first by the word(alphabetically), and then sort them by edit distance
  */
 public class Autocorrect {
 
     /**
-     * Constucts an instance of the Autocorrect class.
+     * Constructs an instance of the Autocorrect class.
      * @param words The dictionary of acceptable words.
      * @param threshold The maximum number of edits a suggestion can have.
      */
-    public Autocorrect(String[] words, int threshold) {
+    public Autocorrect(String[] words, int threshold)
+    {
+//        String testWord = what should be user input
+        String[] dictCopy = words;
+        int thresholdCopy = threshold;
 
+
+
+    }
+
+    public int levenshteinDist(String string1, String string2)
+    {
+        int[][] inspectionTable = new int[string1.length() + 1][string2.length() + 1];
+
+        for(int i = 0; i <= string1.length(); i++)
+        {
+            for(int j = 0; j <= string2.length(); j++)
+            {
+                //Base Case: if length of String a = 0, distance = length of String b.
+                if(i == 0)
+                {
+                    inspectionTable[i][j] = j;
+                }
+                //Base Case: length of String b = 0, distance = length of String a.
+                else if(j == 0)
+                {
+                    inspectionTable[i][j] = i;
+                }
+                //Base Case: if last character of both strings is the same, distance is distance between strings - last character
+                else if(string1.charAt(i - 1) == string2.charAt(j -1))
+                {
+                    inspectionTable[i][j] = inspectionTable[i - 1][j - 1];
+                }
+            }
+        }
+
+        return inspectionTable[string1.length()][string2.length()];
     }
 
     /**
      * Runs a test from the tester file, AutocorrectTester.
      * @param typed The (potentially) misspelled word, provided by the user.
      * @return An array of all dictionary words with an edit distance less than or equal
-     * to threshold, sorted by edit distnace, then sorted alphabetically.
+     * to threshold, sorted by edit distance, then sorted alphabetically.
      */
     public String[] runTest(String typed) {
 
